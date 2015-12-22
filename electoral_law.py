b8f011c68bf4589635ea9ae8b2af8e2efb3fd7b1
Albert Fajula <fajulanet@gmail.com>
2015-12-22 13:22:53 +0100

    Creates class, simplifies methods

--- electoral_law.py
+++ electoral_law.py
@@ -8,46 +8,46 @@ Created on Tue Dec 22 09:56:19 2015
 from __future__ import division
 import numpy as np
 
-def DHont(votes, representatives):
-    #
-    # D'Hont highest avarage method
-    #
-    total_votes=0
-    for i in votes:
-        total_votes+=i
-    #spanish threshod 3%
-    dhont_threshold=total_votes*0.03
+class ley_electoral(object):
     
-    valid_votes=np.array([score for score in votes if score>=dhont_threshold])
-    
-    dhont_chart=np.zeros((valid_votes.size,representatives))
-    
-    for j in xrange(representatives):
-        dhont_chart[:,j]=valid_votes[:]/(j+1)
-    
-    elected=-np.sort(-dhont_chart, axis=None)
-    
-    return elected[:representatives]
+    def __init__ (self, votos, representantes, metodo='DHont', corte=0.03):
+        
+                #self.info = basic_info()
+        self.votes=votos
+        self.total_votes=0
+        
+        for i in self.votes:
+            self.total_votes+=i
+        
+        self.representatives=representantes
+        self.ham=metodo #Highest avarage method
+        self.minratio=corte
+        self.threshold=self.total_votes*self.minratio
+        
+        self.valid_votes=np.array([score for score in self.votes if score>=self.threshold])
+        self.chart=np.zeros((self.valid_votes.size,self.representatives))
+        
+        if self.ham=='DHont':
+            self.DHont()
+        elif self.ham=='Sainte_Lague':
+            self.Sainte_Lague()
+        
+        self.elected=np.argsort(-self.chart, axis=None)
+        
+        self.elected=(self.elected[:self.representatives])
+        self.elected[:]=np.uint8(self.elected[:]/self.representatives)
+        
+        
+    def DHont(self):
+        #
+        # D'Hont highest avarage method
+        #
+        for j in xrange(self.representatives):
+            self.chart[:,j]=self.valid_votes[:]/(j+1)
 
-
-def Sainte_Lague(votes, representatives):
-    #
-    #  Sainte-Laguë Highest avarage method, 
-    #
-    total_votes=0
-    for i in votes:
-        total_votes+=i
-    #spanish threshod 3%
-    webster_threshold=total_votes*0.03
-    
-    valid_votes=np.array([score for score in votes if score>=webster_threshold])
-    
-    webster_chart=np.zeros((valid_votes.size,representatives))
-    
-    for j in xrange(representatives):
-        webster_chart[:,j]=valid_votes[:]/(2*j+1)
-    
-    elected=-np.sort(-webster_chart, axis=None)
-    
-    return elected[:representatives]
-
+    def Sainte_Lague(self):
+        #
+        #  Sainte-Laguë Highest avarage method, 
+        #
+        for j in xrange(self.representatives):
+            self.chart[:,j]=self.valid_votes[:]/(2*j+1)
