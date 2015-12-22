c153b7001e45c95518107ec29b6e8cc56e15dd22
Isman Siete <isman7@gmail.com>
2015-12-22 12:49:28 +0100

    New LUT for Districts

--- eldiarioes-api.py
+++ eldiarioes-api.py
@@ -26,6 +26,64 @@ LIST_OF_ELECTIONS = ['junio-1977',
 LIST_OF_CHAMBERS = ['congreso',
                     'senado']
 
+# orderen decereciente en tamaño:
+
+                    
+LIST_OF_DISTRICS = {u'Madrid':                  'madrid/madrid',
+                    u'Barcelona':               'catalunya/barcelona',
+                    u'Valencia':                'comunitat-valencia/valencia',
+                    u'Alicante':                'comunitat-valencia/alicantealacant',
+                    u'Sevilla':                 'andalucia/sevilla',
+                    u'Málaga':                  'andalucia/malaga',
+                    u'Cádiz':                   'andalucia/cadiz',
+                    u'Bizkaia':                 'euskadi/bizkaia',
+                    u'A Coruña':                'galicia/a-coruna',
+                    u'Illes Balears':           'illes-balears/illes-balears',
+                    u'Las Palmas':              'canarias/las-palmas',
+                    u'Asturias':                'asturias/asturias',
+                    u'Santa Cruz de Tenerife':  'canarias/santa-cruz-de-tenerife',
+                    u'Zaragoza':                'aragon/zaragoza',
+                    u'Pontevedra':              'galicia/pontevedra',
+                    u'Granada':                 'andalucia/granada',
+                    u'Tarragona':               'catalunya/tarragona',
+                    u'Córdoba':                 'andalucia/cordoba',
+                    u'Girona':                  'catalunya/girona',
+                    u'Gipuzkoa':                'euskadi/gipuzkoa',
+                    u'Toledo':                  'castilla-la-mancha/toledo',
+                    u'Almería':                 'andalucia/almeria',
+                    u'Badajoz':                 'extremadura/badajoz',
+                    u'Jaén':                    'andalucia/jaen',
+                    u'Navarra':                 'navarra/navarra',
+                    u'Castellón':               'comunitat-valencia/castelloncastello',
+                    u'Cantabria':               'cantabria/cantabria',
+                    u'Valladolid':              'castilla-y-leon/valladolid',
+                    u'Ciudad Real':             'castilla-la-mancha/ciudad-real',
+                    u'Huelva':                  'castilla-la-mancha/huelva',
+                    u'León':                    'castilla-y-leon/león',
+                    u'Lleida':                  'catalunya/lleida',
+                    u'Cáceres':                 'extremadura/caceres',
+                    u'Albacete':                'castilla-la-mancha/albacete',
+                    u'Burgos':                  'castilla-y-leon/burgos',
+                    u'Salamanca':               'castilla-y-leon/salamanca/',
+                    u'Lugo':                    'galicia/lugo',
+                    u'Ourense':                 'galicia/ourense',
+                    u'La Rioja':                'la-rioja/la-rioja',
+                    u'Álava':                   'euskadi/alava',
+                    u'Guadalajara':             'castilla-la-mancha/guadalajara',
+                    u'Huesca':                  'aragon/huesca',
+                    u'Cuenca':                  'castilla-la-mancha/cuenca',
+                    u'Zamora':                  'castilla-y-leon/zamora',
+                    u'Ávila':                   'castilla-y-leon/avila',
+                    u'Palencia':                'castilla-y-leon/palencia',
+                    u'Segovia':                 'castilla-y-leon/segovia',
+                    u'Teruel':                  'aragon/teruel',
+                    u'Soria':                   'castilla-y-leon/soria',
+                    u'Ceuta':                   'ceuta/ceuta',
+                    u'Melilla':                 'melilla/melilla'
+                    }
+
+
+
 
 class basic_info(object):
     
